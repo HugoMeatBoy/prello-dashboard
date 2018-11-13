2bbb778f3b673d119af9605f2d563f8b65f0e073
Unknown <hugo.fazio@aol.com>
2018-11-13 01:07:59 +0100

    bind data general + install moment.js

--- src/helpers/DataToGeneral.js
+++ src/helpers/DataToGeneral.js
@@ -1,22 +1,41 @@
 import React from 'react';
+import * as moment from 'moment';
+
+
 
 const DataToGeneral = (data) => {
+    moment().format();
+
     if(data.default){
         var cardsLists = data.default.lists;
     }else{
         var cardsLists = data.lists;
     }
+
     var generalInfo = {};
 
     var cardsCpt = 0;
     var cardsCompletedCpt = 0;
-    var firstCreation = '';
-    var lastCreation = '';
+    var firstCreation = undefined;
+    var lastCreation = undefined;
+
+    var membersListTmp = [];
+    
 
     cardsLists.forEach(function(l) {
         cardsCpt += l.cards.length;
         l.cards.forEach(function(c) {
             if(c.isValidated){ cardsCompletedCpt ++; }
+
+            let cardDate = moment(c.creationDate);
+            if(firstCreation === undefined){ firstCreation = cardDate }
+            else if(lastCreation === undefined){ lastCreation = cardDate  }
+            else if(cardDate.isBefore(firstCreation)){ firstCreation = cardDate }
+            else if(cardDate.isAfter(lastCreation)){ lastCreation = cardDate }
+
+            c.members.forEach(function(m){
+                membersListTmp += m
+            });
         })
     });
 
@@ -24,7 +43,9 @@ const DataToGeneral = (data) => {
     generalInfo.listsNumber = cardsLists.length;
     generalInfo.cardsNumber = cardsCpt;
     generalInfo.checklistsCompleted = cardsCompletedCpt / cardsCpt * 100;
-
+    generalInfo.firstCardCreated = firstCreation.format("hh:mm DD/MM/YYYY");
+    generalInfo.lastCardCreated = lastCreation.format("hh:mm DD/MM/YYYY");
+    generalInfo.membersNumber = membersListTmp.length;
 
     return generalInfo;
 
