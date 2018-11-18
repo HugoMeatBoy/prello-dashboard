1a69af50c32525dc247216a16113b0bd45719258
Cyprien Legrand <cyprien_legrand@hotmail.fr>
2018-11-18 16:08:21 +0100

    Remove axes from radar chart

--- src/component/views/BoardOrientationView/index.js
+++ src/component/views/BoardOrientationView/index.js
@@ -1,44 +1,32 @@
 import React from 'react';
-import {Radar} from 'react-chartjs-2';
+import { Radar } from 'react-chartjs-2';
 
 // ===== Others
 import './style.css';
 
 // ==================================
 
-const BoardOrientationView = props =>(
+const BoardOrientationView = props => (
     <div className="col-sm-12 dataType">
         <div className="dataValues">
-            <Radar 
-                data = {props.data}
+            <Radar
+                data={props.data}
                 options={{
                     title: {
                         display: true,
                         text: 'Board orientation',
-                        fontSize: 20
+                        fontSize: 20,
                     },
                     legend: {
-                        display:false
+                        display: false,
                     },
                     maintainAspectRatio: true,
                     circumference: Math.PI,
                     rotation: Math.PI,
-                    scales: {
-						xAxes: [{
-							stacked: true,
-						}],
-						yAxes: [{
-                            stacked: true,
-                            ticks:{
-                                min: 0,
-                                max:8
-                            }
-						}]
-					}
                 }}
             />
         </div>
     </div>
-)
+);
 
-export default BoardOrientationView
+export default BoardOrientationView;
